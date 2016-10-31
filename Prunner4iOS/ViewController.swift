4c468158b630965bbd83cd61083f2703adabac2d
黒澤 預生 <yonamachan3@gmail.com>
2016-11-01 06:42:34 +0900

    Initial Commit

---
+++ Prunner4iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prunner4iOS
+//
+//  Created by 黒澤 預生 on 2016/11/01.
+//  Copyright © 2016年 黒澤 預生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
