f53e58d0b4e7c6c01683faac07c2be424b2860bf
黒澤 預生 <yonama.dev@gmail.com>
2016-11-02 05:53:47 +0900

    現在位置に合わせる

--- Prunner4iOS/ViewController.swift
+++ Prunner4iOS/ViewController.swift
@@ -8,23 +8,41 @@
 
 import UIKit
 import GoogleMaps
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , CLLocationManagerDelegate{
 
+  var locationManager: CLLocationManager?
+  var mapView: GMSMapView?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-    let camera = GMSCameraPosition.camera(withLatitude: -33.868,
-                                                      longitude:151.2086, zoom:6)
-    let mapView = GMSMapView.map(withFrame: CGRect.zero, camera:camera)
+    // 位置情報サービスが利用できるかどうかをチェック
+    if CLLocationManager.locationServicesEnabled() {
+      locationManager = CLLocationManager()
+      locationManager?.delegate = self;
+      // 測位開始
+      locationManager?.startUpdatingLocation();
+    }
     
+    // カメラを作成
+    let camera = GMSCameraPosition.camera(withLatitude: -33.868,
+                                          longitude:151.2086, zoom:6)
+    // ビューを作成
+    mapView = GMSMapView.map(withFrame: CGRect.zero, camera:camera)
+    mapView?.isMyLocationEnabled = true;
+    
+    
+    /*
     let marker = GMSMarker()
     marker.position = camera.target
     marker.snippet = "Hello World"
     marker.appearAnimation = kGMSMarkerAnimationPop
     marker.map = mapView
-    
+    */
+
     self.view = mapView
   }
 
@@ -33,6 +51,22 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    switch status {
+    case .notDetermined:
+      manager.requestWhenInUseAuthorization()
+    case .restricted, .denied:
+      break
+    case .authorizedAlways, .authorizedWhenInUse:
+      break
+    }
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let newLocation = locations.last, CLLocationCoordinate2DIsValid(newLocation.coordinate) else {
+      return
+    }
+    mapView?.animate(toLocation: newLocation.coordinate)
+  }
 }
 
