e1b103936890e9047f072b670e87705f3fcb56de
黒澤 預生 <yonama.dev@gmail.com>
2016-11-02 11:18:59 +0900

    AlamofireとSwiftyJSONの追加

---
+++ Prunner4iOS/Views/PrunnerMapView/PrunnerMapView.swift
@@ -0,0 +1,21 @@
+//
+//  PrunnerMapView.swift
+//  Prunner4iOS
+//
+//  Created by 黒澤 預生 on 2016/11/02.
+//  Copyright © 2016年 黒澤 預生. All rights reserved.
+//
+
+import UIKit
+
+class PrunnerMapView: GMSMapView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
