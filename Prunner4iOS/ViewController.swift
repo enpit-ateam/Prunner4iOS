4ce2633fbb0ffd375700d07cc7f47727e2cfb501
kobayang <naokiod@gmail.com>
2016-11-26 03:12:13 +0900

    fix map functions

--- Prunner4iOS/ViewController.swift
+++ Prunner4iOS/ViewController.swift
@@ -132,29 +132,93 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
     mapView.camera = camera
   }
   
+  private func getPlaces(in distance: Double, latitude: Double, longitude: Double) -> [Place]? {
+    // 指定されたLat,LngからのPlaceリストを返す
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let request = GMPlaceRequest.NearBySearch()
+    var places: [Place]?
+    request.queryParameters = [
+      "key": appDelegate.apiKey as AnyObject,
+      "location": String.init(format: "%f,%f", latitude, longitude) as AnyObject,
+      "radius": String.init(format: "%f", distance) as AnyObject
+    ]
+    
+    // GCDを使って同期処理を実装したいけど、なんかむりっぽい
+    // http://qiita.com/kazuhirox/items/9ecb25bc238ad2d47ff0
+    
+    // ロックの取得
+    var keepAlive = true
+    
+    Session.send(request) { result in
+      switch result {
+      case .success(let response):
+        print(response)
+        places = response
+      case .failure(let error):
+        print("error: \(error)")
+      }
+      keepAlive = false
+    }
+    
+    let runLoop = RunLoop.current
+    while keepAlive &&
+      runLoop.run(mode: RunLoopMode.defaultRunLoopMode, before: NSDate(timeIntervalSinceNow: 0.1) as Date) {
+        // ロックが解除されるまで待つ
+    }
+    return places
+  }
+  
+  private func pickBestPlace(places: [Place], distance: Double) -> Place {
+    // 一番Distanceに近いPlaceを返す
+    let sortedPlaces = places.sorted {(place1 : Place, place2 : Place) -> Bool in
+      let lc1 = CLLocationCoordinate2DMake((place1.geometry.location.lat)!, (place1.geometry.location.lng)!)
+      let d1 = fabs(self.calcCoordinatesDistance(lc1: lc1, lc2: self.mapView.camera.target) - distance)
+      let lc2 = CLLocationCoordinate2DMake((place2.geometry.location.lat)!, (place2.geometry.location.lng)!)
+      let d2 = fabs(self.calcCoordinatesDistance(lc1: lc2, lc2: self.mapView.camera.target) - distance)
+      return d1 < d2
+    }
+    return sortedPlaces[0]
+  }
+
   private func calcCoordinatesDistance(lc1: CLLocationCoordinate2D, lc2: CLLocationCoordinate2D) -> CLLocationDistance {
+    // ふたりの距離の概算
     let l1 = CLLocation(latitude: lc1.latitude, longitude: lc1.longitude)
     let l2 = CLLocation(latitude: lc2.latitude, longitude: lc2.longitude)
     return l1.distance(from: l2)
   }
-
-  private func drawRoute(place: Place){
-     let appDelegate = UIApplication.shared.delegate as! AppDelegate
-     var request = GMDirectionRequest()
-     request.queryParameters = [
-     "origin": String.init(format: "%f,%f", mapView.camera.target.latitude, mapView.camera.target.longitude) as AnyObject,
-     "destination": String.init(format: "%f,%f", mapView.camera.target.latitude+1.0, mapView.camera.target.longitude+1.0) as AnyObject,
-     "key": appDelegate.apiKey as AnyObject
-     ]
-     Session.send(request) { result in
-       switch result {
-       case .success(let response):
-         let direction = response
-         print(direction)
-       case .failure(let error):
-         print("error: \(error)")
-     }
+  
+  private func getDirection(place: Place) -> Direction? {
+    // ルートの取得
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let request = GMDirectionRequest()
+    request.queryParameters = [
+      "origin": String.init(format: "%f,%f", mapView.camera.target.latitude, mapView.camera.target.longitude) as AnyObject,
+      "destination": String.init(format: "%f,%f", place.geometry.location.lat, place.geometry.location.lng) as AnyObject,
+      "key": appDelegate.apiKey as AnyObject
+    ]
+    
+    // ロックの取得
+    var keepAlive = true
+    
+    var direction: Direction?
+    Session.send(request) { result in
+      switch result {
+      case .success(let response):
+        print(response)
+        direction = response
+      case .failure(let error):
+        print("error: \(error)")
+      }
+      
+      keepAlive = false
     }
+    
+    let runLoop = RunLoop.current
+    while keepAlive &&
+      runLoop.run(mode: RunLoopMode.defaultRunLoopMode, before: NSDate(timeIntervalSinceNow: 0.1) as Date) {
+        // ロックが解除されるまで待つ
+    }
+    return direction
   }
 }
 
