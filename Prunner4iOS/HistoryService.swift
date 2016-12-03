05c7cb07ddd2768338bcb0e3c8eafd5d2b496731
rerost <gmailii@yahoo.co.jp>
2016-12-04 01:06:41 +0900

    Named test data

--- Prunner4iOS/HistoryService.swift
+++ Prunner4iOS/HistoryService.swift
@@ -12,33 +12,25 @@ class HistoryService {
   private static let userDefaults = UserDefaults.standard
   
   private static var histories: Histories = []
+  private static var forTestHistories: Histories = [
+    History(date: Date(),
+            //route: nil,
+      distance: 114.5141919,
+      time: 10000),
+    History(date: Date(),
+            //route: nil,
+      distance: 314.159265,
+      time: 10000)
+  ]
   
   class func getHistories() -> Histories{
     userDefaults.register(defaults: ["DataStore": "default"])
     let storedData:Data? = userDefaults.object(forKey: "History") as! Data?
     guard storedData != nil else{
-      return [
-        History(date: Date(),
-                //route: nil,
-          distance: 114.5141919,
-          time: 10000),
-        History(date: Date(),
-                //route: nil,
-          distance: 314.159265,
-          time: 10000)
-      ]
+      return forTestHistories
     }
     guard let hs = NSKeyedUnarchiver.unarchiveObject(with: storedData as! Data!) as! Histories? else {
-      return [
-        History(date: Date(),
-                //route: nil,
-                distance: 114.5141919,
-                time: 10000),
-        History(date: Date(),
-                //route: nil,
-                distance: 314.159265,
-                time: 10000)
-      ]
+      return forTestHistories
     }
     histories = hs
     return histories
