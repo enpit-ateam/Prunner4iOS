b7e806cdf30dd88589f0bbedde6e82f1da0efdac
rerost <gmailii@yahoo.co.jp>
2016-12-14 22:41:40 +0900

    数字と文字を分離した

--- Prunner4iOS/Views/TableView/TableView.swift
+++ Prunner4iOS/Views/TableView/TableView.swift
@@ -19,30 +19,31 @@ class TableView: UITableViewCell {
    */
   
   // Labels
+  
   @IBOutlet weak var dayLabel: UILabel!
   @IBOutlet weak var timeLabel: UILabel!
   @IBOutlet weak var distanceLabel: UILabel!
   @IBOutlet weak var runTimeLabel: UILabel!
   @IBOutlet weak var paceLabel: UILabel!
   
-  /*@IBOutlet weak var dayLabel: UILabel!
-  @IBOutlet weak var timeLabel: UILabel!
-  @IBOutlet weak var distanceLabel: UILabel!
-  @IBOutlet weak var runTimeLabel: UILabel!
-  @IBOutlet weak var paceLabel: UILabel!*/
+//  @IBOutlet weak var dayLabel: UILabel!
+//  @IBOutlet weak var timeLabel: UILabel!
+//  @IBOutlet weak var distanceLabel: UILabel!
+//  @IBOutlet weak var runTimeLabel: UILabel!
+//  @IBOutlet weak var paceLabel: UILabel!
   
   func setCell(history: History) {
     let distance: Double = history.distance! / 1000.0
     let time: Int = history.time!
     let hour: Int = time / (60 * 60)
     let minute: Int = (time % (60 * 60)) / 60
-    let pace: Int = Int(distance / Double(time == 0 ? 1 : time))
+    let pace: Int = Int(distance / Double((time == 0 ? 1 : time)))
     let startDate = history.date!
     let day:Int = DayService.getComponent(date: startDate).day!
-    dayLabel.text = day.description + "日"
-    timeLabel.text = String(format: "%d:%2d", DayService.getComponent(date: startDate).hour!, DayService.getComponent(date: startDate).minute!)
-    distanceLabel.text = String(format: "%.3f", distance)
-    runTimeLabel.text = String(format: "%d:%2d分", hour, minute)
+    dayLabel.text = day.description
+    timeLabel.text = String(format: "%2d:%2d", DayService.getComponent(date: startDate).hour!, DayService.getComponent(date: startDate).minute!)
+    distanceLabel.text = String(format: "%.1f", distance)
+    runTimeLabel.text = String(format: "%d:%2d", hour, minute)
     paceLabel.text = String(format: "%d", pace)
   }
 }
